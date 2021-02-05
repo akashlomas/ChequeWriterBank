6d60f53477c0cc688087a736097e597d805dc7ef
Akash Lomas <akash@netsolutions.com>
2021-02-05 18:21:13 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ChequeWriterBank.Models;
+
+namespace ChequeWriterBank.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AmountViewModel model)
+        {
+            string isNegative = "";
+            try
+            {
+                var amount = Convert.ToDouble(model.NumberAmount).ToString();
+
+                if (amount.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    amount = amount.Substring(1, amount.Length - 1);
+                }
+                if (amount == "0")
+                {
+                    ModelState.AddModelError("", "The number in currency fomat is \nZero Only");
+                    return View(model);
+                }
+                else
+                {
+                    model.WordAmount = isNegative + ConvertToWords(amount);
+                }
+                return View("Cheque", model);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IActionResult Cheque(AmountViewModel amountViewModel)
+        {
+            return View(amountViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        #region private_methods
+        private static string Ones(string Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String name = "";
+            switch (_Number)
+            {
+
+                case 1:
+                    name = "One";
+                    break;
+                case 2:
+                    name = "Two";
+                    break;
+                case 3:
+                    name = "Three";
+                    break;
+                case 4:
+                    name = "Four";
+                    break;
+                case 5:
+                    name = "Five";
+                    break;
+                case 6:
+                    name = "Six";
+                    break;
+                case 7:
+                    name = "Seven";
+                    break;
+                case 8:
+                    name = "Eight";
+                    break;
+                case 9:
+                    name = "Nine";
+                    break;
+            }
+            return name;
+        }
+
+        private static string Tens(string Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String name = null;
+            switch (_Number)
+            {
+                case 10:
+                    name = "Ten";
+                    break;
+                case 11:
+                    name = "Eleven";
+                    break;
+                case 12:
+                    name = "Twelve";
+                    break;
+                case 13:
+                    name = "Thirteen";
+                    break;
+                case 14:
+                    name = "Fourteen";
+                    break;
+                case 15:
+                    name = "Fifteen";
+                    break;
+                case 16:
+                    name = "Sixteen";
+                    break;
+                case 17:
+                    name = "Seventeen";
+                    break;
+                case 18:
+                    name = "Eighteen";
+                    break;
+                case 19:
+                    name = "Nineteen";
+                    break;
+                case 20:
+                    name = "Twenty";
+                    break;
+                case 30:
+                    name = "Thirty";
+                    break;
+                case 40:
+                    name = "Fourty";
+                    break;
+                case 50:
+                    name = "Fifty";
+                    break;
+                case 60:
+                    name = "Sixty";
+                    break;
+                case 70:
+                    name = "Seventy";
+                    break;
+                case 80:
+                    name = "Eighty";
+                    break;
+                case 90:
+                    name = "Ninety";
+                    break;
+                default:
+                    if (_Number > 0)
+                    {
+                        name = Tens(Number.Substring(0, 1) + "0") + " " + Ones(Number.Substring(1));
+                    }
+                    break;
+            }
+            return name;
+        }
+
+        private static string ConvertWholeNumber(string Number)
+        {
+            string word = "";
+            try
+            {
+                bool beginsZero = false;
+                bool isDone = false;
+                double dblAmt = (Convert.ToDouble(Number));
+
+                if (dblAmt > 0)
+                {
+                    beginsZero = Number.StartsWith("0");
+
+                    int numDigits = Number.Length;
+                    int pos = 0;
+                    string place = "";
+                    switch (numDigits)
+                    {
+                        case 1:
+
+                            word = Ones(Number);
+                            isDone = true;
+                            break;
+                        case 2:
+                            word = Tens(Number);
+                            isDone = true;
+                            break;
+                        case 3:
+                            pos = (numDigits % 3) + 1;
+                            place = " Hundred ";
+                            break;
+                        case 4://thousands' range    
+                        case 5:
+                        case 6:
+                            pos = (numDigits % 4) + 1;
+                            place = " Thousand ";
+                            break;
+                        case 7://millions' range    
+                        case 8:
+                        case 9:
+                            pos = (numDigits % 7) + 1;
+                            place = " Million ";
+                            break;
+                        case 10://Billions's range    
+                        case 11:
+                        case 12:
+
+                            pos = (numDigits % 10) + 1;
+                            place = " Billion ";
+                            break;
+                        //add extra case options for anything above Billion...    
+                        default:
+                            isDone = true;
+                            break;
+                    }
+                    if (!isDone)
+                    {//if transalation is not done, continue...(Recursion comes in now!!)    
+                        if (Number.Substring(0, pos) != "0" && Number.Substring(pos) != "0")
+                        {
+                            try
+                            {
+                                word = ConvertWholeNumber(Number.Substring(0, pos)) + place + ConvertWholeNumber(Number.Substring(pos));
+                            }
+                            catch (Exception ex)
+                            {
+                                throw ex;
+                            }
+                        }
+                        else
+                        {
+                            word = ConvertWholeNumber(Number.Substring(0, pos)) + ConvertWholeNumber(Number.Substring(pos));
+                        }
+                    }
+                    //ignore digit grouping names    
+                    if (word.Trim().Equals(place.Trim())) word = "";
+                }
+            }
+            catch { }
+            return word.Trim();
+        }
+
+        private static string ConvertToWords(string numb)
+        {
+            string wholeNo = numb, andStr = "", pointStr = "";
+            string endStr = "Only";
+            string val;
+            try
+            {
+                int decimalPlace = numb.IndexOf(".");
+                if (decimalPlace > 0)
+                {
+                    wholeNo = numb.Substring(0, decimalPlace);
+                    string points = numb.Substring(decimalPlace + 1);
+                    if (Convert.ToInt32(points) > 0)
+                    {
+                        andStr = "and";// just to separate whole numbers from points/cents    
+                        endStr = "Paisa " + endStr;//Cents    
+                        pointStr = ConvertDecimals(points);
+                    }
+                }
+                val = string.Format("{0} {1}{2} {3}", ConvertWholeNumber(wholeNo).Trim(), andStr, pointStr, endStr);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return val;
+        }
+
+        private static string ConvertDecimals(string number)
+        {
+            string cd = "";
+            for (int i = 0; i < number.Length; i++)
+            {
+                string digit = number[i].ToString();
+                string engOne;
+                if (digit.Equals("0"))
+                {
+                    engOne = "Zero";
+                }
+                else
+                {
+                    engOne = Ones(digit);
+                }
+                cd += " " + engOne;
+            }
+            return cd;
+        }
+        #endregion
+    }
+}
---
+++ Models/AmountViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChequeWriterBank.Models
+{
+    public class AmountViewModel
+    {
+        public string NumberAmount { get; set; }
+        public string WordAmount { get; set; }
+
+    }
+}
